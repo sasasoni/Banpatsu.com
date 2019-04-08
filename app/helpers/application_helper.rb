33e8e59244af48e688ed6348ae445a1373f6aff5
sasasoni <chibichibi1997@hotmail.co.jp>
2019-04-08 18:13:29 +0000

    Fix google_calendar_url helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,8 +17,8 @@ module ApplicationHelper
   def google_calendar_url(event)
     base_url = "http://www.google.com/calendar/event?action=TEMPLATE&"
     start_date = event.start_date.strftime("%Y%m%dT%H%M%S")
-    end_date = event.end_date.nil? ? start_date : event.end_date.strftime("%Y%m%dT%H%M%S")
-    event_url = event.event_url.nil?  ? "" : "%0D%0A" + "URL:" + event.event_url
+    end_date = event.end_date.blank? ? start_date : event.end_date.strftime("%Y%m%dT%H%M%S")
+    event_url = event.event_url.blank?  ? "" : "%0D%0A" + "URL:" + event.event_url
     base_url + "text=#{event.title}&details=#{truncate(event.description, lengh: 80)}#{event_url}&dates=#{start_date}/#{end_date}"
   end
 end
