930675167c033f8c5b287ef77717a60beb09a1ec
sasasoni <chibichibi1997@hotmail.co.jp>
2019-04-16 13:42:07 +0000

    Fix Calendar dayNames

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -147,6 +147,7 @@ $(document).on('turbolinks:load', function () {
 
     $('#calendar').fullCalendar({
       events: '/events/calendar.json',
+      dayNames: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
       dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
       header: {
         left: 'month,agendaWeek,agendaDay',
